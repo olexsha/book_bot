5a17f06d09197a2276b433a274f2a87673cdc82d
Aleksei Pavlov <lexus.msu@gmail.com>
2024-04-08 16:10:41 +0200

    Initial commit

--- base.py
+++ base.py
@@ -2,11 +2,15 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+import json
+
+with open('config.json') as f:
+    data = json.load(f)
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="7098867664:AAEOnSom48NM2NrXSEVnaW7YEQMsUpy5hkw")
+bot = Bot(token=data['bot_token']['token'])
 # Диспетчер
 dp = Dispatcher()
 
