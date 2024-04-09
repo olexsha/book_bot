aa655070e56c75a220b375a03b04337f23c308c0
Aleksei Pavlov <lexus.msu@gmail.com>
2024-04-09 16:27:39 +0200

    reorganise

--- log_test.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.WARNING)
-
-logging.debug('Это лог уровня DEBUG')
-logging.info('Это лог уровня INFO')
-logging.warning('Это лог уровня WARNING')
-logging.error('Это лог уровня ERROR')
-logging.critical('Это лог уровня CRITICAL')
